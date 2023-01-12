a586abf5695f9a64f8ebcef01dac34899ed1925a
BeefyBombarossa <gaboiscool@gmail.com>
2023-01-12 15:39:35 -0500

    Created the basic structure for the tic tac toe game thing

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -8,6 +8,16 @@ author, and this description to match your project!
 
 "use strict";
 
+let spacing = 40;
+let topX = 150;
+let topY = 150;
+//1 = X 2 = O
+
+let myArray = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 2, 2]
+    ];
 
 /**
 Description of preload
@@ -23,6 +33,9 @@ Description of setup
 function setup() {
     createCanvas(500, 500);
     background(0, 255, 0);
+    let donkey = "DONKEY";
+    myArray.push(donkey);
+    myArray.push("PENIS");
 }
 
 
@@ -30,5 +43,20 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    //line 1
+    text(myArray[0][0], topX, topY);
+    text(myArray[0][1], topX + spacing, topY);
+    text(myArray[0][2], topX + spacing*2, topY);
+
+    //line 2
+    text(myArray[1][0], topX, topY+ spacing);
+    text(myArray[1][1], topX + spacing, topY+ spacing);
+    text(myArray[1][2], topX + spacing*2,topY+ spacing);
+    
+    //line 3
+    text(myArray[2][0], topX, topY+ spacing*2);
+    text(myArray[2][1], topX + spacing, topY+ spacing*2);
+    text(myArray[2][2], topX + spacing*2,topY+ spacing*2);
+    
 
 }

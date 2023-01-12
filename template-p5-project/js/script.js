39ead5ad229937d04d8374cd6567883f8df4f49f
BeefyBombarossa <gaboiscool@gmail.com>
2023-01-12 16:05:48 -0500

    Made the boxes pretty and aligned them

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -8,9 +8,13 @@ author, and this description to match your project!
 
 "use strict";
 
-let spacing = 40;
-let topX = 150;
-let topY = 150;
+let size = 200;
+let topX = 50;
+let topY = 50;
+
+let winWidth = (size*3)+(topX+topY);
+let winHeight = (size*3)+(topX+topY);
+
 //1 = X 2 = O
 
 let myArray = [
@@ -31,32 +35,64 @@ function preload() {
 Description of setup
 */
 function setup() {
-    createCanvas(500, 500);
-    background(0, 255, 0);
+    createCanvas(winWidth, winHeight);
+    background(125);
     let donkey = "DONKEY";
     myArray.push(donkey);
     myArray.push("PENIS");
 }
 
+function drawBoard() {
+
+//GRID
+    push();
+
+    fill(200);
+    rect(topX, topY, size, size);
+    rect(topX+size, topY, size, size);
+    rect(topX+size*2, topY, size, size);
+
+    rect(topX, topY +size, size, size);
+    rect(topX + size, topY+size, size, size);
+    rect(topX + size*2, topY+size, size, size);
+
+    rect(topX, topY+size*2, size, size);
+    rect(topX+size, topY+size*2, size, size);
+    rect(topX+size*2, topY+size*2, size, size);
+
+    pop();
+
+//INSIDE BOXES
+    //line 1
+      
+    push();
+    topX += size/2;
+    topY += size/2;
+
+    fill(50)
+    text(myArray[0][0], topX, topY);
+    text(myArray[0][1], topX + size, topY);
+    text(myArray[0][2], topX + size*2, topY);
+
+    //line 2
+    text(myArray[1][0], topX, topY+ size);
+    text(myArray[1][1], topX + size, topY+ size);
+    text(myArray[1][2], topX + size*2,topY+ size);
+
+    //line 3
+    text(myArray[2][0], topX, topY+ size*2);
+    text(myArray[2][1], topX + size, topY+ size*2);
+    text(myArray[2][2], topX + size*2,topY+ size*2);
+
+    pop();
+
+    topX = 50;
+    topY = 50;
+}
 
 /**
 Description of draw()
 */
 function draw() {
-    //line 1
-    text(myArray[0][0], topX, topY);
-    text(myArray[0][1], topX + spacing, topY);
-    text(myArray[0][2], topX + spacing*2, topY);
-
-    //line 2
-    text(myArray[1][0], topX, topY+ spacing);
-    text(myArray[1][1], topX + spacing, topY+ spacing);
-    text(myArray[1][2], topX + spacing*2,topY+ spacing);
-    
-    //line 3
-    text(myArray[2][0], topX, topY+ spacing*2);
-    text(myArray[2][1], topX + spacing, topY+ spacing*2);
-    text(myArray[2][2], topX + spacing*2,topY+ spacing*2);
-    
-
+    drawBoard();
 }
